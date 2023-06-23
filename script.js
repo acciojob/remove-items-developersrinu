0586123179cb49317f4933b965ab3198a06ce1a7
developersrinu <132943635+developersrinu@users.noreply.github.com>
2023-06-24 01:30:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+
+const colorSelect = document.getElementById('colorSelect');
+const removeButton = document.getElementById('removeButton');
+removeButton.addEventListener('click', function() {
+  const selectedIndex = colorSelect.Index;
+  colorSelect.remove(Index);
+});
