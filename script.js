889c32fb9bb464cf76ae00255ba952e6f3682c1d
developersrinu <132943635+developersrinu@users.noreply.github.com>
2023-06-24 01:54:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-
+// Get the dropdown list element
 const colorSelect = document.getElementById('colorSelect');
+
+// Get the button element
 const removeButton = document.getElementById('removeButton');
+
+// Add event listener to the button
 removeButton.addEventListener('click', function() {
-  const selectedIndex = colorSelect.Index;
-  colorSelect.remove(Index);
+  // Get the selected index of the dropdown list
+  const selectedIndex = colorSelect.selectedIndex;
+
+  // Remove the selected option from the dropdown list
+  colorSelect.remove(selectedIndex);
 });
